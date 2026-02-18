79fc021d9ed7c767991f9c34a71e0162e99742ef
Quang Tran <qdtran.dev@gmail.com>
2026-02-18 18:53:13 -0500

    dynamic-colorchange feature

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: DigitalPetApp(),
+  ));
+}
+
+class DigitalPetApp extends StatefulWidget {
+  @override
+  _DigitalPetAppState createState() => _DigitalPetAppState();
+}
+
+class _DigitalPetAppState extends State<DigitalPetApp> {
+  String petName = "Destroyer";
+  int happinessLevel = 50;
+  int hungerLevel = 50;
+
+  // Dynamic Color Change (ColorFiltered)
+  Color _moodColor(double happinessLevel) {
+    if (happinessLevel > 70) {
+      return Colors.green;
+    } else if (happinessLevel >= 30) {
+      return Colors.yellow;
+    } else {
+      return Colors.red;
+    }
+  }
+
+  void _playWithPet() {
+    setState(() {
+      happinessLevel += 10;
+      _updateHunger();
+    });
+  }
+
+  void _feedPet() {
+    setState(() {
+      hungerLevel -= 10;
+      _updateHappiness();
+    });
+  }
+
+  void _updateHappiness() {
+    if (hungerLevel < 30) {
+      happinessLevel -= 20;
+    } else {
+      happinessLevel += 10;
+    }
+  }
+
+  void _updateHunger() {
+    setState(() {
+      hungerLevel += 5;
+      if (hungerLevel > 100) {
+        hungerLevel = 100;
+        happinessLevel -= 20;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digital Pet'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Pet image tinted based on happiness
+            ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                _moodColor(happinessLevel.toDouble()),
+                BlendMode.modulate,
+              ),
+              child: Image.asset(
+                'assets/pet_image.png',
+                width: 160,
+                height: 160,
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(height: 20.0),
+
+            Text('Name: $petName', style: TextStyle(fontSize: 20.0)),
+            SizedBox(height: 16.0),
+            Text('Happiness Level: $happinessLevel', style: TextStyle(fontSize: 20.0)),
+            SizedBox(height: 16.0),
+            Text('Hunger Level: $hungerLevel', style: TextStyle(fontSize: 20.0)),
+            SizedBox(height: 32.0),
+            ElevatedButton(
+              onPressed: _playWithPet,
+              child: Text('Play with Your Pet'),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: _feedPet,
+              child: Text('Feed Your Pet'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
